82a283adf05695696a1d4688bd123f0349c556da
Fortunate-MAN <shivashish.ahuja@gmail.com>
2017-08-06 10:42:28 +0530

    removing obsolete code

--- QuietRooms.py
+++ QuietRooms.py
@@ -2,55 +2,6 @@ import chatexchange
 import Utilities
 import Chatcommunicate
 
-"""
-quietRooms = list()
-
-def getQuietRoomByID (roomID):
-    for each_room in quietRooms:
-        if each_room ["room_id"] == roomID:
-            return each_room
-
-    return None
-
-def addQuietRoom (roomID, interval=120):
-    if isRoomQuiet (roomID) == True:
-        return
-    
-    room = Utilities.client.get_room(roomID)
-    room.join()
-    room.watch_polling (Chatcommunicate.handleMessage, interval)
-
-    Utilities.rooms.append (room)
-
-    quietRooms.append ({"room_id": roomID, "interval": interval})
-
-def deleteQuietRoom (roomID):
-    if isRoomQuiet (roomID) == False:
-        return
-
-    for each_room in Utilities.rooms:
-        if each_room.id == roomID:
-            each_room.leave()
-            Utilities.rooms.remove (each_room)
-            break
-
-    quietRoom = getQuietRoomByID(roomID)
-    quietRooms.remove (quietRoom)
-
-def isRoomQuiet (roomID):
-    for each_room in quietRooms:
-        if each_room ["room_id"] == roomID:
-            return True
-
-    return False
-
-def saveQuietRoomList ():
-    Utilities.saveToPickle ("quiet_rooms.pickle", quietRooms)
-
-def loadQuietRoomList ():
-    return Utilities.loadFromPickle ("quiet_rooms.pickle")
-"""
-
 quiet_rooms = list()
 
 def add_quiet_room(room_id, join=True, interval=120):
--- TrackBots.py
+++ TrackBots.py
@@ -5,57 +5,6 @@ import string
 import pickle
 import Utilities
 
-"""
-botsList = [{"name": "", "user_id": -1, "to_ping": "", "time_to_wait": 900, "last_message_time": time.time(), "rooms": [], "status": ""}]
-#botsList = list()
-
-def addBot (name, user_id, to_ping, rooms, time_to_wait=900, last_message_time=time.time(), status="alive"):
-    botsList.append ({"name": name, "user_id": user_id, "to_ping": to_ping, "time_to_wait": time_to_wait, "last_message_time": last_message_time, "rooms": rooms, "status": status})
-"""
-"""
-def updateLastMessageTime (userID):
-    for each_bot in botsList:
-        if each_bot ["user_id"] == userID:
-            print ("Last message time for userID '" + str (each_bot["user_id"]) + "' has been updated.")
-            each_bot ["last_message_time"] = time.time()
-            each_bot ["status"] = "alive"
-            break
-
-def updateTimeToWait (userID, timeToWait):
-    for each_bot in botList:
-        if each_bot ["user_id"] == userID:
-            each_bot ["time_to_wait"] = timeToWait
-            break
-
-def deleteBot (userID):
-    for each_bot in botsList:
-        if each_bot ["user_id"] == userID:
-            botsList.remove (each_bot)
-
-def isBotAlive (userID):
-    alive = False
-
-    for each_bot in botsList:
-        if each_bot ["user_id"] == userID:
-            if time.time() - each_bot ["last_message_time"] < each_bot ["time_to_wait"]:
-                alive = True
-
-    return alive
-
-def getBotIndexByID (userID):
-    for i in range (len (botsList)):
-        if botsList [i]["user_id"] == userID:
-            return i
-
-    return -1
-
-def saveBotList ():
-    Utilities.saveToPickle ("bot_list.pickle", botsList)
-
-def loadBotList ():
-    return Utilities.loadFromPickle ("bot_list.pickle")
-"""
-
 bots_list = []
 
 def add_bot(name, user_id, to_ping, rooms, time_to_wait=900, last_message_time=time.time(), alive=True):
